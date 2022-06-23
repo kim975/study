1098364fbbd3e9e3398bd79ec9ee4f0d5caeae10
kim <scilo200@gmail.com>
2022-06-24 00:08:45 +0900

    no message

--- src/main/java/com/example/study/controller/HelloController.java
+++ src/main/java/com/example/study/controller/HelloController.java
@@ -1,7 +1,12 @@
 package com.example.study.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.bind.DatatypeConverter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @RestController
 public class HelloController {
@@ -9,5 +14,23 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello!";
+    }
+
+    @GetMapping("/hash/{input}")
+    public String getDigest(@PathVariable("input") String input) throws NoSuchAlgorithmException {
+        for(int i = 0; i < 100_000; i++) {
+            input = getMD5Digest(input);
+        }
+        return input;
+    }
+
+    private String getMD5Digest(String input) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        md.update(input.getBytes());
+        byte[] digest = md.digest();
+        String myHash = DatatypeConverter
+                .printHexBinary(digest).toUpperCase();
+
+        return myHash;
     }
 }
