92e1cda98aa57f51143f27b799344a51374ccd91
kim975 <81149759+kim975@users.noreply.github.com>
2025-01-05 16:01:51 +0900

    알고리즘 문제 풀이
    프로그래머스
    문자열 내 p와 y의 개수 : https://programmers.co.kr/learn/courses/30/lessons/12916

---
+++ algorithm/src/programmers/문자열_내_p와_y의_개수.java
@@ -0,0 +1,30 @@
+package programmers;
+
+/**
+ * 문자열 내 p와 y의 개수
+ * https://programmers.co.kr/learn/courses/30/lessons/12916
+ */
+public class 문자열_내_p와_y의_개수 {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.solution(new int[][]{{0, 1, -1}, {1, 0, -1}, {1, 0, 1}});
+    }
+
+    private static class Solution {
+        boolean solution(String s) {
+            boolean answer = true;
+            int temp = s.length();
+            s = s.toLowerCase();
+
+            s = s.replaceAll("p", "");
+
+            int pCount = temp - s.length();
+            temp = s.length();
+            s = s.replaceAll("y", "");
+            int yCount = temp - s.length();
+
+            return pCount == yCount;
+        }
+    }
+}
