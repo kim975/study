c8752b776b9234b5ecff31347ff1c90569800643
kim975 <81149759+kim975@users.noreply.github.com>
2025-01-16 15:36:02 +0900

    알고리즘 문제 풀이
    백준
    저작권 : https://www.acmicpc.net/problem/2914

---
+++ algorithm/src/baekjoon/저작권.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * 저작권 : https://www.acmicpc.net/problem/2914
+ */
+public class 저작권 {
+
+}
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        System.out.println(A * (B - 1) + 1);
+    }
+}
